7e8e05f0e25c746aa656651a74a265c2bb00b035
Alexey Zhuravlev <zhuravlevab@yandexlyceum.ru>
2025-05-22 17:43:27 +0300

    The design of the text for the console was added and a build of the program was made.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import os, io
-from enum import Enum
-
+import os
 import openpyxl
+
+from enum import Enum
 from docxtpl import DocxTemplate
 from docx2pdf import convert
+from art import tprint
 
 
 class CertType(Enum):
@@ -25,26 +26,45 @@ def get_dir(directory: str):
 
 
 def make_certificate(tpl_data: dict, cert_type: CertType, path: str) -> None:
-    certificate = get_template(cert_type)
+    docx_dir = get_dir(f"{path}/docx")
     file_name = f"{tpl_data["surname"]} {tpl_data["name"]} {tpl_data["patronymic"]}"
-    certificate.render(tpl_data)
-    certificate.save(f"{path}/{file_name}.docx")
 
-    docx_file = f"{path}/{file_name}.docx"
-    pdf_file = f"{path}/{file_name}.pdf"
+    certificate = get_template(cert_type)
+    certificate.render(tpl_data)
+    certificate.save(f"{docx_dir}/{file_name}.docx")
+
+    pdf_dir = get_dir(f"{path}/pdf")
+    docx_file = f"{docx_dir}/{file_name}.docx"
+    pdf_file = f"{pdf_dir}/{file_name}.pdf"
     convert(docx_file, pdf_file)
 
 
 WORKING_DIR = get_dir("сертификаты")
 CERT_DATA_SHEET = "cert_data"
 
-course_dir = WORKING_DIR
 tpl_data_keys = ("surname", "name", "patronymic", "course", "mod", "hour", "cert", "number")
 
 wb = openpyxl.load_workbook(filename="data/IT-куб.xlsx")
 
 
+def print_info(count: int):
+    border = "█" * 50
+    print(border)
+    print("   Распечатано сертификатов всего:", count)
+    print("   Из них:")
+    for sheet in wb.sheetnames:
+        if sheet == 'cert_data':
+            continue
+
+        active_sheet = wb[sheet]
+        print(f"         ├ {sheet}: {len(list(active_sheet.iter_rows())) - 1}")
+    print(border)
+
+
 def main():
+    tprint('starting...')
+    count = 0
+
     for sheet in wb.sheetnames:
         if sheet == CERT_DATA_SHEET:
             continue
@@ -52,7 +72,7 @@ def main():
         try:
             course_dir = get_dir(f"{WORKING_DIR}/{sheet}")
         except OSError as e:
-            print(f"Не могу создать папку в папке '{WORKING_DIR}' для листа '{sheet}'.")
+            print(f"Не могу создать папку курса в папке '{WORKING_DIR}' для листа '{sheet}'.")
             print(f"Возникла ошибка: {e}")
             break
 
@@ -69,11 +89,18 @@ def main():
                 if edu_module != "без модуля":
                     context['course'] = f"{context['course']} ({edu_module})"
 
-                make_certificate(context, CertType(context["cert"]), course_dir)
+                try:
+                    make_certificate(context, CertType(context["cert"]), course_dir)
+                    count += 1
+                except Exception as e:
+                    print(f"При создании сертификата возникла ошибка: {e}")
 
         except Exception as e:
-            print(f"При формировании набора данных возникла ошибка:\n{e}")
+            print(f"При формировании набора данных возникла ошибка: {e}")
 
+    tprint('done!')
+    print_info(count)
+    input()
 
 if __name__ == "__main__":
     main()
