866c66123e447ca48281dfbfb5b6f2ba4179ecb7
Alexey Zhuravlev <zhuravlevab@yandexlyceum.ru>
2025-06-03 11:03:12 +0300

    Implemented a selection of sheets for printing

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from enum import Enum
 from docxtpl import DocxTemplate
 from docx2pdf import convert
 from art import tprint
+from openpyxl import Workbook
 
 
 class CertType(Enum):
@@ -42,47 +43,46 @@ def make_certificate(tpl_data: dict, cert_type: CertType, path: str) -> None:
     convert(docx_file, pdf_file)
 
 
+def pages_range(pages: str):
+    result = []
+    for item in pages.replace(' ', '').split(','):
+        if '-' in item:
+            start, end = map(int, item.split('-'))
+            result.extend(range(start, end + 1))
+        else:
+            result.append(int(item))
+    return result
+
+
+def get_used_sheets(sheets_range: list[int], wb: Workbook):
+    using_sheets_names = []
+    for number, name in enumerate(wb.sheetnames, 1):
+        if number in sheets_range:
+            using_sheets_names.append(name)
+    return using_sheets_names
+
+
 WORKING_DIR = get_dir("сертификаты")
 CERT_DATA_SHEET = "cert_data"
 
 tpl_data_keys = ("surname", "name", "patronymic", "course", "mod", "hour", "cert", "number")
-
 wb = openpyxl.load_workbook(filename="data/IT-куб.xlsx")
 
 
-def print_info(count: int):
-    border = "█" * 50
-    print(border)
-    print("   Распечатано сертификатов всего:", count)
-    print("   Из них:")
-    for sheet in wb.sheetnames:
-        if sheet == 'cert_data':
-            continue
-
-        active_sheet = wb[sheet]
-        print(f"         ├ {sheet}: {len(list(active_sheet.iter_rows())) - 1}")
-    print(border)
-
-
 def main():
-    rows = 3
-    row = 0
     for num, sheet in enumerate(wb.sheetnames, 1):
-        row += 1
-        print(f"{num}) {sheet};", end='\t')
-        if row % rows == 0:
-            row = 0
-            print()
+        if sheet == CERT_DATA_SHEET:
+            continue
+        print(f'[{num}]{sheet}; ', end='')
 
-    print("Какие листы использовать?")
-    use_sheets_numbers = int(input())
-    return
+    sheets_range = pages_range(input("\nКакие листы использовать? "))
+    work_sheets = get_used_sheets(sheets_range, wb)
 
     tprint('starting...')
     count = 0
 
     for sheet in wb.sheetnames:
-        if sheet == CERT_DATA_SHEET:
+        if sheet not in work_sheets:
             continue
 
         try:
@@ -115,8 +115,8 @@ def main():
             print(f"При формировании набора данных возникла ошибка: {e}")
 
     tprint('done!')
-    print_info(count)
-    input()
+    input("Нажмите любую кнопку, чтобы закрыть это окно.")
+
 
 if __name__ == "__main__":
     main()
